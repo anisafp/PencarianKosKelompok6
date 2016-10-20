8b37091cb8bf31539c5c97015cb4ec4571ebceb6
anisafp <anisafitrianti111@gmail.com>
2016-10-20 10:35:32 +0700

    pertama

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl406162636/pencariankoskelompok6/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl406162636.pencariankoskelompok6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
